c322ee5811eea1ddc993cb3dd96a6327828c36bb
dani <ahmaddani25.mhs@akb.ac.id>
2026-06-03 13:31:38 +0700

    perbaikan

--- config/db.config.js
+++ config/db.config.js
@@ -1,20 +1,25 @@
 import { Sequelize } from "sequelize";
-import mysql2 from "mysql2";
- const db = new Sequelize('web_lanjut', 'avnadmin', 'AVNS_nafzc8AF5RebBM4S5oY', {
- host: "web-lanjut-weblanjut.h.aivencloud.com",
- dialect: "mysql",
- port: 10209,
- dialectOptions: { 
-ssl: { 
-rejectUnauthorized: false 
-} 
-},
- "define": {
- "timestamps": false
- }
- });
- export default db;
 
+const db = new Sequelize(
+  "defaultdb",
+  "avnadmin",
+  "AVNS_nafzc8AF5RebBM4S5oY",
+  {
+    host: "web-lanjut-weblanjut.h.aivencloud.com",
+    dialect: "mysql",
+    port: 10209,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      timestamps: false,
+    },
+  }
+);
+
+export default db;
 
 /* (async()=>{
      await db.sync();
